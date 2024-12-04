8228647c86d7e73c96c3bb185e3c16179e0f3c59
mazenhaouari40 <mazenhaouari97@gmail.com>
2024-12-04 23:18:27 +0100

    minimum error

--- A - Find Minimum Operations.cpp
+++ A - Find Minimum Operations.cpp
@@ -1,1 +1,48 @@
 
+#include<iostream>
+using namespace std;
+#include<cmath>
+
+
+int main(){
+int t,n,k,min_op;
+cin>>t;
+while(t--){
+    cin>>n>>k;
+
+        if (k ==1 ){
+            cout << n<<endl;
+        }
+        else{
+
+                if ((n % k ==0) ){
+                    cout << 1<<endl;
+                }
+                else{
+
+                 min_op=0;
+                 int x=0;
+                 while(pow(k,x) <= n){
+                    x++;
+                 }
+                 x--;
+                 int d = pow(k,x);
+                 while(n !=0){
+                    if (n>=d){
+                    n = n - d;
+                    min_op++;
+                    }
+                    else{
+                        d=d/k;
+                    }
+
+                 }
+                 cout<<min_op<<endl;
+        }
+    }
+
+
+}
+}
+
+
