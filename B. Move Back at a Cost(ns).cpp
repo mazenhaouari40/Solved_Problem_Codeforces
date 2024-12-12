331e8ed7d6e08ca96c296f4e0ac7bedd6f460f4e
mazenhaouari40 <mazenhaouari97@gmail.com>
2024-12-12 17:51:20 +0100

    no solved problem move back a cost

---
+++ B. Move Back at a Cost(ns).cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+
+bool verifie(vector<int> t,int ele,int deb,int fin){
+
+for(int i=deb;i<fin;i++){
+    if (t[i] == ele){
+        return true;
+    }
+}
+return false;
+}
+
+
+void solve(){
+    int n,x;
+    cin>>n;
+    vector<int> a;
+    for (int i=0;i<n;i++){
+        cin>>x;
+        a.push_back(x);
+    }
+    vector<int> trie_a= a;
+    sort(trie_a.begin(),trie_a.end());
+    int i=0,nb=0;
+    while( nb<n+1 ){
+        if (a[i] == trie_a[i]){
+            i++;
+        }
+        else
+        {
+            if (verifie(a,trie_a[i],i,n)){
+                x = a[i]+1;
+                a.erase(a.begin()+i);
+                a.push_back(x);
+            }
+            else
+            {
+                break;
+            }
+        }
+        nb++;
+    }
+
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+
+
+}
+
+
+
+
+
+int main(){
+int t;
+cin>>t;
+while(t--){
+    solve();
+}
+}
